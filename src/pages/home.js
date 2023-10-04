c6dc58e6d19e845e278cb1066c4c7e8e6ef571fc
Joe Simpson <josephsimpson94@googlemail.com>
2023-10-04 15:36:39 +0100

    added faker

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,17 @@
+import { faker } from "@faker-js/faker";
+
 const Home = (props) => {
   return (
     <div>
       <h1>Homepage</h1>
       {props.allCats.map((cat, index) => {
+        const randomName = faker.person.fullName();
+        const randomPrice = faker.commerce.price();
         return (
           <div key={index}>
             <img src={cat.url} alt="Cat" />
+            <p>{randomName}</p>
+            <p>£{randomPrice}</p>
           </div>
         );
       })}
