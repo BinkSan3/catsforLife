f7f1413fd8578360813f70c0c5c906d66cb47b64
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-10-06 16:33:15 +0100

    Final CSS

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -14,22 +14,25 @@ const CartModal = ({
 }) => {
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose}>
-      <div>
-        <h1>Shopping Cart</h1>
+    <Modal className="random" isOpen={isOpen} onRequestClose={onClose}>
+      <div id="closeButton">
+        <h2>Shopping Cart</h2>
         <button onClick={onClose}>Close</button>
       </div>
 
       {cart.map((cat, index) => (
-        <div key={index}>
+        <div id="catStuffs" key={index}>
           <p>{cat.name}</p> <p>£{cat.price}</p>
           <button onClick={() => removeFromCart(cat, index)}>Remove</button>
         </div>
       ))}
 
       <h3>Your Total: £{totalPrice}</h3>
-      <button onClick={onClose}>Continue shopping</button>
-      <button onClick={() => checkout(cart)}>Checkout</button>
+
+      <div id="buttonsDiv">
+        <button onClick={onClose}>Continue shopping</button>
+        <button onClick={() => checkout(cart)}>Checkout</button>
+      </div>
     </Modal>
   );
 };
