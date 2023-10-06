104f5c01920bdeb925e853681c0600a44794ef32
Joe Simpson <josephsimpson94@googlemail.com>
2023-10-06 09:38:32 +0100

    disabling button attempt

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Modal from "react-modal";
 
-const Cart = ({ cart }) => {
+const CartModal = ({ isOpen, onClose, addToCart, removeFromCart, cart }) => {
   return (
-    <div>
+    <Modal isOpen={isOpen} onRequestClose={onClose}>
       <h1>Shopping Cart</h1>
-      {cart.map((cat, index) => (
-        <div key={index}>{/* Display cart item information */}</div>
-      ))}
-    </div>
+      <ul>
+        {cart.map((cat) => (
+          <li key={cat.id}>
+            {cat.name} - {cat.price}
+            <button onClick={() => removeFromCart(cat.id)}>Remove</button>
+            <button onClick={() => addToCart(cat.id)}>Add</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={onClose}>Close</button>
+    </Modal>
   );
 };
 
-export default Cart;
+export default CartModal;
